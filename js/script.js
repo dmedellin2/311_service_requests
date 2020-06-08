1f166fbbe3ec3ff16bbcdd7f45718a06caa965ac
Danielle Medellin <daniellemedellin2@gmail.com>
2020-06-08 18:30:20 -0400

    updated slides

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$("#complaint_type").change ( function () {
-    var targID  = $(this).val ();
-    $("div.style-sub-1").hide ();
-    $('#' + targID).show ();
-} )
-
-
-https://css-tricks.com/couple-takes-sticky-footer/
